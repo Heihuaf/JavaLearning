f70aa087488d7bb5b338d7e60b79dda2aa5e2051
Yi <97965124+Heihuaf@users.noreply.github.com>
2023-07-08 00:15:06 +0800

    Create HelloWorld.java

---
+++ Practices/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.ember.base;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        //单行注释
+        //输出一个Hello，World!
+        System.out.println("Hello,World!");
+
+        //多行注释
+        /*
+        多行
+        注释
+         */
+
+        //JavaDoc:文档注释 /**   */
+        /**
+         * @Desciption Base.HelloWorld
+         * @Author Ember
+         */
+    }
+}
